79257dfd8ed6e98967959408d4286b4126da64e8
gpx <1457495424@qq.com>
2018-09-05 21:10:47 +0800

    add P1914

---
+++ P1914.cpp
@@ -0,0 +1,34 @@
+/*************************************************************************
+	> File Name: P1914.cpp
+	> Author: gpx 
+	> Mail: 1457495424@qq.com 
+	> Created Time: 2018年09月05日 星期三 20时56分11秒
+ ************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 50
+
+char password[MAX + 5] = {0};
+
+int main() {
+    int n, i = 0;
+    char c;
+    scanf("%d", &n);
+    getchar();
+    scanf("%c", &c);
+    while (c != '\n') {
+        password[i] = c;
+        i++;
+        scanf("%c", &c);
+    }
+    for (int j = 0; j < i; j++) {
+        if (password[j] == 'z') password[j] = 'a';
+        else password[j] = password[j] + 1;
+    }
+    for (int j = 0; j < i; j++) {
+        printf("%c", password[j]);
+    }
+    printf("\n");
+    return 0;
+}
