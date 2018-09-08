527d3af9e5d68597547871eb267b10dea371c50c
gpx <1457495424@qq.com>
2018-09-08 21:57:08 +0800

    modify P1149.cpp

--- P1149.cpp
+++ P1149.cpp
@@ -12,19 +12,24 @@ int num[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
 
 int valid(int n) {
     if (n < 10) return num[n];
-    return num[n / 10] + num[n % 10];
+    int a = 0;
+    while (n) {
+        a += num[n % 10];
+        n /= 10;
+    }
+    return a;
 }
 
 int main() {
     int n, ans = 0;
     scanf("%d", &n);
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+    for (int i = 0; i <= 1111; i++) {
+        for (int j = 0; j <= 1111; j++) {
             if (i == j) continue;
             if (valid(i) + valid(j) + valid(i + j) == n - 4) ans += 1;
         }
     }
-    for (int i = 0; i < n/2; i++) {
+    for (int i = 0; i <= 1111; i++) {
         if (valid(i) + valid(i) + valid (i + i) == n - 4) ans += 1;
     }
     printf("%d\n", ans);
