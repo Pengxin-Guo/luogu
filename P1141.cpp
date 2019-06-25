46e3a366bc09966ac4fd531217f50850a71cc1d8
gpx <1457495424@qq.com>
2019-06-25 20:34:32 +0800

    add P1141.cpp

--- P1141.cpp
+++ P1141.cpp
@@ -4,17 +4,21 @@
 	> Mail: 1457495424@qq.com
 	> Created Time: 2019年06月18日 星期二 20时47分41秒
  ************************************************************************/
+// TLE
 
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <queue>
 using namespace std;
 #define MAX_N 1000
-#define MAX_M 100000
 
 char maze[MAX_N + 5][MAX_N + 5];
 int n, m;
 int vis[MAX_N + 5][MAX_N + 5];
+int map[MAX_N + 5][MAX_N + 5];
+int ans[MAX_N * MAX_N + 5];
 
 typedef struct Node {
     int x, y;
@@ -24,16 +28,18 @@ int in(int x, int y) {
     return x >= 0 && x < n && y >= 0 && y < n;
 }
 
-int bfs(int i, int j) {
+int bfs(int id, int i, int j) {
     int cnt = 0;
     memset(vis, 0, sizeof(vis));
     queue<Node> q;
     q.push({i, j});
     vis[i][j] = 1;
+    map[i][j] = id;
     while (!q.empty()) {
         Node temp = q.front();
-        int x = temp.x, y = temp.y;
         q.pop();
+        int x = temp.x, y = temp.y;
+        map[x][y] = id;
         cnt++;
         if (maze[x][y] == '0') {
             if (in(x + 1, y) && !vis[x + 1][y] && maze[x + 1][y] == '1') vis[x + 1][y] = 1, q.push({x + 1, y});
@@ -55,10 +61,15 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> maze[i];
     }
+    int id = 1;
     while (m--) {
         int x, y;
         cin >> x >> y;
-        cout << bfs(x - 1, y - 1) << endl;
+        if (vis[x - 1][y - 1] == 0) {
+            ans[id] = bfs(id, x - 1, y - 1);
+            id++;
+        }
+        cout << ans[map[x - 1][y - 1]] << endl;
     }
     return 0;
 }
