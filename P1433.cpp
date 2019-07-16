ad26688a073f1bcf040b054bc2c5b7589fd4a612
gpx <1457495424@qq.com>
2019-07-16 21:22:35 +0800

    add P1433.cpp

--- P1433.cpp
+++ P1433.cpp
@@ -17,34 +17,46 @@ typedef struct Node {
 }Node;
 
 Node arr[MAX_N + 5];
+double dis[MAX_N + 5][MAX_N + 5];
 int n;
-int index[MAX_N + 5];
+int vis[MAX_N + 5];
 double ans = 0x3f3f3f3f;
 
 void init() {
-    for (int i = 0; i <= n; i++) index[i] = i;
+    for (int i = 1; i <= n; i++) {
+        cin >> arr[i].x >> arr[i].y;
+    }
+    // 预处理一下点与点之间的距离
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
+            dis[i][j] = sqrt((arr[i].x - arr[j].x) * (arr[i].x - arr[j].x) + (arr[i].y - arr[j].y) * (arr[i].y - arr[j].y));
+        }
+    }
     return ;
 }
 
-void func() {
-    double temp = 0.0;
-    for (int i = 0; i < n; i++) {
-        int id1 = index[i], id2 = index[i + 1];
-        temp += sqrt((arr[id1].x - arr[id2].x) * (arr[id1].x - arr[id2].x) + (arr[id1].y - arr[id2].y) * (arr[id1].y - arr[id2].y));
+void dfs(int step, int now, double length) {
+    if (length > ans) {    // 剪枝
+        return ;
     }
-    ans = min(ans, temp);
+    if (step == n) {
+        ans = min(ans, length);
+        return ;
+    }
+    for (int i = 1; i <= n; i++) {
+        if (!vis[i]) {
+            vis[i] = 1;
+            dfs(step + 1, i, length + dis[now][i]);
+            vis[i] = 0;
+        }
+    }
     return ;
 }
 
 int main() {
     cin >> n;
     init();
-    for (int i = 1; i <= n; i++) {
-        cin >> arr[i].x >> arr[i].y;
-    }
-    do {
-        func();
-    } while (next_permutation(index + 1, index + 1 + n));
+    dfs(0, 0, 0.0);
     printf("%.2f\n", ans);
     return 0;
 }
